c16650ca27c4691a7ce0e76a96311e7307386398
i-curve <i-curve@qq.com>
2023-11-13 01:49:16 +0800

    remove div function
    Signed-off-by: i-curve <i-curve@qq.com>

--- include/mathpp/mathpp.hh
+++ include/mathpp/mathpp.hh
@@ -3,4 +3,3 @@
 int sum(int a, int b);
 int sub(int a, int b);
 int mul(int a, int b);
-int div(int a, int b);
--- src/mathpp.cpp
+++ src/mathpp.cpp
@@ -9,4 +9,3 @@
 int sum(int a, int b) { return a + b; }
 int sub(int a, int b) { return a - b; }
 int mul(int a, int b) { return a * b; }
-int div(int a, int b) { return a / b; }
